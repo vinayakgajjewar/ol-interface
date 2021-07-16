cfe01d081d75e1f8d2450aa5d2424dbe034eea57
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2021-07-16 10:14:58 -0700

    rename some variables

--- index.js
+++ index.js
@@ -56,21 +56,42 @@ var highlightStyle = new Style({
 
 // county data is only for california
 // place data is only for la/riverside/san diego area
+// zcta5 data is only for riverside
 var statePath = "data/geojson/TIGER2018_STATE_data_index.geojson"
 var countyPath = "data/geojson/TIGER2018_COUNTY_california.geojson"
 var placePath = "data/geojson/TIGER2018_PLACE_riverside.geojson"
+var zcta5Path = "data/geojson/TIGER2018_ZCTA5_riverside.geojson"
 
-// read and parse geojson file
-var geoJSONString = fs.readFileSync(placePath, {
+// read and parse geojson files
+var stateGeoJSONString = fs.readFileSync(statePath, {
   encoding: "utf8",
   flag: "r"
 })
-var geoJSONObj = JSON.parse(geoJSONString)
+var stateGeoJSONObj = JSON.parse(stateGeoJSONString)
 
-var vectorLayer = new VectorLayer({
+var countyGeoJSONString = fs.readFileSync(countyPath, {
+  encoding: "utf8",
+  flag: "r"
+})
+var countyGeoJSONObj = JSON.parse(countyGeoJSONString)
+
+var placeGeoJSONString = fs.readFileSync(placePath, {
+  encoding: "utf8",
+  flag: "r"
+})
+var placeGeoJSONObj = JSON.parse(placeGeoJSONString)
+
+var zcta5GeoJSONString = fs.readFileSync(zcta5Path, {
+  encoding: "utf8",
+  flag: "r"
+})
+var zcta5GeoJSONObj = JSON.parse(zcta5GeoJSONString)
+
+// make vector layers using geojson objs
+var stateVectorLayer = new VectorLayer({
   source: new VectorSource({
     // featureProjection: "EPSG:3857"} is necessary for the code to work with UCR-Star data
-    features: new GeoJSON({featureProjection: "EPSG:3857"}).readFeatures(geoJSONObj)
+    features: new GeoJSON({featureProjection: "EPSG:3857"}).readFeatures(stateGeoJSONObj)
   }),
   style: function(feature) {
     style.getText().setText(feature.get("NAME"))
@@ -82,7 +103,7 @@ var vectorLayer = new VectorLayer({
 var map = new Map({
   layers: [new TileLayer({
     source: new OSM()
-  }), vectorLayer],
+  }), stateVectorLayer],
   target: "map",
   view: new View({
     center: [0, 0],
